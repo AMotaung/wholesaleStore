f908c7b3d1e6c1a9ef87631b808336022588a486
AMotuang <alfredthabang@gmail.com>
2022-10-26 13:30:12 +0200

    Product Crud functions

--- src/main/java/com/store/wholesaleStore/controller/ProductController.java
+++ src/main/java/com/store/wholesaleStore/controller/ProductController.java
@@ -3,33 +3,39 @@ package com.store.wholesaleStore.controller;
 import com.store.wholesaleStore.model.Product;
 import com.store.wholesaleStore.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
 
+@RestController
 @Controller
 public class ProductController {
 
     @Autowired
     ProductService productService;
 
-
+    //Adding a Product
     @PostMapping("/products/addproduct")
-    public String createProduct(Product product, Model model, BindingResult bindingResult, HttpServletRequest request, HttpServletResponse response) {
+    public ResponseEntity<String> createProduct(@RequestBody Product product, Model model, BindingResult bindingResult, HttpServletRequest request, HttpServletResponse response) {
         if(bindingResult.hasErrors()) {
             try {
                 response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Cannot save Product");
             }catch(IOException e) {
                 e.printStackTrace();
             }
-            return "products/addproduct";
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("All fields required.Make sure your input is valid.");
         }else {
 
             DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
@@ -38,11 +44,39 @@ public class ProductController {
 
             product.setCreatedAt(date);
             product.setModified(date);
-
             productService.addProduct(product);
-            request.setAttribute("product", product);
-            return "";
+         //   request.setAttribute("product", product);
+          System.out.println(product.toString());
+
+            return ResponseEntity.ok("Product added successfully");
         }
     }
+
+    //Getting all Products
+    @GetMapping(value = "getallproducts")
+    public List<Product>getProducts(HttpServletRequest request){
+        return productService.getAll();
+    }
+
+    //Getting Product by ID
+    @GetMapping(value = "/getproduct/{id}")
+    public Optional<Product> findProductById(@PathVariable(name = "id", required = true) int id){
+        return productService.getProductById(id);
+    }
+
+    //Delete Product by ID
+    @GetMapping(value = "/product/{id}/delete")
+    public void removeProductById(@PathVariable(name = "id", required = true) int id){
+        productService.deleteProduct(id);
+    }
+
+
+    //Deleting All Products
+    @GetMapping(value = "/products/delete")
+    public void removeAllProducts(){
+         productService.deleteAllProducts();
+    }
+
+
 
 }
